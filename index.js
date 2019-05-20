2c52691947505acad4431a3d9e180220d3784514
Noah Feinberg <noahafeinberg@icloud.com>
2019-05-19 18:06:12 -0700

    first

---
+++ index.js
@@ -0,0 +1,38 @@
+var memeClient = require('./meme-eco-wrap')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var token = "NTc5ODIzOTA3NjI0NzE0MjQw.XOHxIw.urU8e-aqILTajGB93kx3u1paHOg";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if(msg.content.startsWith('!getfirm')){
+    var args = msg.toString().split(" ")
+    memeClient.getFirm(args[1],firm=>msg.channel.send(`
+    name: ${firm.name}
+    firm balance: ${firm.balance}
+    private: ${firm.private}
+    execs: ${firm.execs}
+    members: ${firm.size}
+    tax: ${firm.tax}%
+    last payout: ${firm.last_payout}
+    `));
+
+  }
+  if (msg.content.startsWith('!getuser')) {
+    var args = msg.toString().split(" ")
+    memeClient.getUser(args[1],user=>msg.channel.send(`
+    name: ${user.name}
+    balance: ${user.balance}
+    completed investments: ${user.completed}
+    times gone broke: ${user.broke}
+    networth: ${user.networth}
+    firm id: ${user.firm}
+    ranking: ${user.rank}`))
+  }
+});
+
+client.login(token);
---
+++ meme-eco-wrap.js
@@ -0,0 +1,30 @@
+var request = require('request')
+
+module.exports = {
+    getUser: function (username, cb) {
+        var options = {
+            url: `https://meme.market/api/investor/${username}`,
+            headers: {
+                'referer': `https://meme.market/user.html?account=${username}`
+            }
+        }
+        request(options, function (err, res, body) {
+            var json = JSON.parse(body);
+            cb(json)
+        });
+    },
+
+    getFirm: function (firmid, cb) {
+        var options = {
+            url: `https://meme.market/api/firm/${firmid}`,
+            headers: {
+                'referer': `https://meme.market/user.html?account=creativityisdying`
+            }
+        }
+        request(options, function (err, res, body) {
+            var json = JSON.parse(body);
+            cb(json)
+        });
+    }
+}
+
