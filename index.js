9b13aa175cbb1b2923bcf40d77f196d022927aaf
Redacted <40678304+noah427@users.noreply.github.com>
2019-05-21 12:39:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,80 @@
-var request = require('request')
+var memeClient = require('./meme-eco-wrap')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var keepAlive = require('./keepalive');
 
-module.exports = {
-  getUser: function(username, cb) {
-    var options = {
-      url: `https://meme.market/api/investor/${username}`,
-      headers: {
-        'referer': `https://meme.market/user.html?account=${username}`
-      }
-    }
-    request(options, function(err, res, body) {
-      var json = JSON.parse(body);
-      cb(json)
-    });
-  },
+var prefix = process.env.PREFIX;
+var token = process.env.TOKEN;
+var useKeepAlive = process.env.USEKEEPALIVE;
 
-  getFirm: function(firmid, cb) {
-    var options = {
-      url: `https://meme.market/api/firm/${firmid}`,
-      headers: {
-        'referer': `https://meme.market/user.html?account=creativityisdying`
-      }
-    }
-    request(options, function(err, res, body) {
-      var json = JSON.parse(body);
-      cb(json)
-    });
-  },
-  getInvestments: function(username, cb) {
-    var options = {
-      url: `https://meme.market/api/investor/${username}/investments?`,
-      headers: {
-        'referer': `https://meme.market/user.html?account=${username}`
-      }
-    }
-    request(options, function(err, res, body) {
-      var json = JSON.parse(body);
-      cb(json)
-    });
-  },
-
-  unixToDate: function timeConverter(UNIX_timestamp) {
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
-    return time;
-  }
+if (useKeepAlive === "true") {
+  keepAlive.run()
 }
 
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`prefix is: ${prefix}`)
+});
+
+client.on('message', msg => {
+  if (msg.content.startsWith(`${prefix}help`)) {
+    msg.channel.send("```json" + `
+    ${prefix}help: shows this message
+    ${prefix}getfirm: gets firm stats by id
+    ${prefix}getuser: gets user stats by username
+    ${prefix}getinvestments: gets a users last three investments by username
+    `+ "```")
+  }
+  if (msg.content.startsWith(`${prefix}getfirm`)) {
+    var args = msg.toString().split(" ")
+
+    memeClient.getFirm(args[1], firm => msg.channel.send("```js\n" + `
+    name: ${firm.name}
+    firm balance: ${firm.balance}
+    Private: ${firm.private}
+    Execs: ${firm.execs}
+    Members: ${firm.size}
+    Tax: ${firm.tax}%
+    `+ "```"));
+  }
+  if (msg.content.startsWith(`${prefix}getinvestments`)) {
+    var args = msg.toString().split(" ")
+    memeClient.getInvestments(args[1], investments => {
+      var countdown = 3
+      investments.forEach(element => {
+        countdown--
+        if (countdown >= 0) {
+          var investmentData = `
+        Investment: 
+        Completed: ${element.done}
+        Amount: ${element.amount}
+        Successful: ${element.success}
+        Starting upvotes: ${element.upvotes}
+        Final upvotes: ${element.final_upvotes}
+        Profit: ${element.profit}
+        Id: ${element.id}
+        Comment: ${element.comment}
+        Post: ${element.post}
+        Response: ${element.response}
+        Time: ${memeClient.unixToDate(element.time)}
+        `
+          msg.channel.send("```js\n" + investmentData + "```")
+        }
+      });
+    })
+  }
+  if (msg.content.startsWith(`${prefix}getuser`)) {
+    var args = msg.toString().split(" ")
+    memeClient.getUser(args[1], user => msg.channel.send("```js\n" + `
+    Name: ${user.name}
+    Balance: ${user.balance}
+    Completed investments: ${user.completed}
+    Times gone broke: ${user.broke}
+    Net worth: ${user.networth}
+    Firm id: ${user.firm}
+    Ranking: ${user.rank}` + "```"))
+  }
+});
+
+client.login(token);
